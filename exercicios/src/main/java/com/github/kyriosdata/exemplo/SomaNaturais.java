af4cf8ee95e98274f6a69ff0cedf7464b54dddd6
Inf UFG <inf@inf.ufg.br>
2016-09-28 20:13:32 -0300

    aula2

--- exercicios/src/main/java/com/github/kyriosdata/exemplo/SomaNaturais.java
+++ exercicios/src/main/java/com/github/kyriosdata/exemplo/SomaNaturais.java
@@ -1,6 +1,31 @@
+/*
+ * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+
 package com.github.kyriosdata.exemplo;
 
+import java.time.LocalDate;
+
+/**
+ * Implementação do cálculo da soma vários de números naturais.
+ *
+ * @author Marcos Mathias Pereira
+ *
+ */
+
 public class SomaNaturais {
+	
+	/**
+     * Obtém a soma de uma quantidade de números naturais.
+     * 
+     * @param n A quantidade de números naturais que se deseja somar, à partir de 1. 
+     * Ex: Se desejamos somar os 5 primeiros números naturais, n=5.
+     *
+     * @return O valor da soma.
+
+     * @throws IllegalArgumentException Se "n" não for um número natural.
+     */
 	
 	public static int soma(int n){
 		
